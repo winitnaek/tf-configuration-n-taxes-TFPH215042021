52bb850ec378c344af405ea64ed99c55ece593ab
Peter Fullen <peterf@bsihq.com>
2020-02-03 17:04:19 -0500

    Worked on making Grid reusable

--- src/app/home/AddressOverrides.js
+++ src/app/home/AddressOverrides.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import Grid from '../components/Grid';
+
+let isOpen = false;
+
+const toggleModal = () => {
+  
+    isOpen = !isOpen;
+    console.log(isOpen)
+}
+
 
 const AddressOverrides = () => {
-    return <h1> Address Overrides</h1>;
-}
- 
+  return (
+    <div>
+      <h1> Address Overrides</h1>
+      <Grid open={isOpen} close={toggleModal}>
+        <h1>  Title passed as a prop</h1>
+
+
+        <p> More info passed as a prop</p>
+
+      </Grid>
+    </div>
+  );
+};
+
 export default AddressOverrides;
--- src/app/home/fontAwesome.js
+++
@@ -1,3 +0,0 @@
-import {faStar} from '@fortawesome/free-solid-svg-icons'
-
-export default faStar;
