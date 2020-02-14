db974ad9f417517e3b30c8432c9fa1710ddb85aa
Vinit Naik <vnaik@bsihq.com>
2020-02-14 14:06:15 -0500

    updated

---
+++ src/base/constants/TFTools.js
@@ -0,0 +1,4 @@
+export const tftools = [
+{ "value": 'CP', "label": 'Custom Payments', "desc":'Custom Payments'},
+{ "value": 'CT', "label": 'Custom Tax Codes', "desc":'Custom Tax Codes'},
+{ "value": 'UQ', "label": 'User Queries', "desc":'User Data Queries'}];
