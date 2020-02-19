fedf9ca597e36342781b317ffc6ad925c44020ca
Vinit Naik <vnaik@bsihq.com>
2020-02-19 12:21:37 -0500

    Updated to handle click from pg

--- src/base/constants/TFTools.js
+++ src/base/constants/TFTools.js
@@ -1,8 +1,10 @@
 import {customPayments,customTaxCodes,allBSIPlans,populateV3States} from '../../app/metadata/metaData';
 export const tftools = [
-{ "value": 'CP', "label": 'Custom Payments', "desc":'Custom Payments', "id":'customPayments', type:'comp'},
-{ "value": 'CT', "label": 'Custom Tax Codes', "desc":'Custom Tax Codes', "id":'customTaxCodes', type:'comp'},
-{ "value": 'UQ', "label": 'User Queries', "desc":'User Data Queries', "id":'userDataQueries', type:'page'}];
+{ "value": 'CP', "label": 'Custom Payments', "desc":'Custom Payments', "id":'customPayments', type:'comp', link:true},
+{ "value": 'CT', "label": 'Custom Tax Codes', "desc":'Custom Tax Codes', "id":'customTaxCodes', type:'comp',link:true},
+{ "value": 'UQ', "label": 'User Queries', "desc":'User Data Queries', "id":'userDataQueries', type:'page',link:true},
+{ "value": 'UQ', "label": 'All BSI Plans', "desc":'All BSI Plans', "id":'allBSIPlans', type:'comp',link:false},
+{ "value": 'UQ', "label": 'Populate V3 States', "desc":'Populate V3 States', "id":'populateV3States', type:'comp',link:false}];
 export const metadatamap = [
     {"id":'customPayments', metadata:customPayments},
     {"id":'customTaxCodes', metadata:customTaxCodes},
