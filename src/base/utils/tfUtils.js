82ae3ec54a768344526cf2b69a3b75041b48273b
Vinit Naik <vnaik@bsihq.com>
2020-02-27 11:32:20 -0500

    Updated for some more cleanup

--- src/base/utils/tfUtils.js
+++ src/base/utils/tfUtils.js
@@ -1,40 +1,68 @@
-import {tftools} from '../constants/TFTools';
-class tfUtils {
-  /**
-   * buildModuleAreaLinks
-   * @param {*} apps 
-   */
-  static buildModuleAreaLinks(apps) {
-    let premTFtools = [];
-    apps.forEach(function(app) {
-      if (app.id == "73b9a516-c0ca-43c0-b0ae-190e08d77bcc") {
-        app.accessIds.forEach(function(access) {
-          if (access.id == "162ebe14-8d87-44e1-a786-c9365c9d5cd8" && access.visible == true) {
-            premTFtools = tftools.filter(tftool => { if (app.permissions.hasOwnProperty(tftool.value) && tftool.link==true) return tftool} ); 
-             
-            
-          }
-        });
-      }
-    });
-    return premTFtools;
-  }
-  /**
-   * setPerms
-   * @param {*} perm 
-   */
-  static setPerms(perm) {
-    let appperm = {
-      VIEW: perm[0] == 1 ? true : false,
-      SAVE: perm[1] == 1 ? true : false,
-      DELETE: perm[2] == 1 ? true : false,
-      RUN: perm[3] == 1 ? true : false,
-      AUDIT: perm[4] == 1 ? true : false
-    };
-    return appperm;
-  }
-  static openHelp(pageid){
-    window.open('/help/'+pageid,'_blank');
+import { metadatamap, tftools } from "../constants/TFTools";
+/**
+ * buildModuleAreaLinks
+ * @param {*} apps
+ */
+export function buildModuleAreaLinks(apps) {
+  let premTFtools = [];
+  apps.forEach(function(app) {
+    if (app.id == "73b9a516-c0ca-43c0-b0ae-190e08d77bcc") {
+      app.accessIds.forEach(function(access) {
+        if (
+          access.id == "162ebe14-8d87-44e1-a786-c9365c9d5cd8" &&
+          access.visible == true
+        ) {
+          premTFtools = tftools.filter(tftool => {
+            if (
+              app.permissions.hasOwnProperty(tftool.value) &&
+              tftool.link == true
+            )
+              return tftool;
+          });
+        }
+      });
+    }
+  });
+  return premTFtools;
+}
+/**
+ * setPerms
+ * @param {*} perm
+ */
+export function setPerms(perm) {
+  let appperm = {
+    VIEW: perm[0] == 1 ? true : false,
+    SAVE: perm[1] == 1 ? true : false,
+    DELETE: perm[2] == 1 ? true : false,
+    RUN: perm[3] == 1 ? true : false,
+    AUDIT: perm[4] == 1 ? true : false
+  };
+  return appperm;
+}
+/**
+ * openHelp
+ * @param {*} pageid
+ */
+export function openHelp(pageid) {
+  window.open("/help/" + pageid, "_blank");
+}
+/**
+ * compMetaData
+ * @param {*} pageid
+ */
+export function compMetaData(pageid) {
+  let metadataMap = metadatamap.find(metadatam => {
+    if (pageid == metadatam.id) return metadatam;
+  });
+  return metadataMap.metadata;
+}
+/**
+ * compPermissions
+ * @param {*} pid
+ */
+export function compPermissions(pid) {
+  let perms = getAllRights();
+  if (perms.hasOwnProperty(pid)) {
+    return setPerms(perms[pid]);
   }
 }
-export default tfUtils;
