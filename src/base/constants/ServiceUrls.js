d493bdcf1c27ac560d45037ba507f021356a9046
Vinit Naik <vnaik@bsihq.com>
2020-02-28 22:25:27 -0500

    Added urls

--- src/base/constants/ServiceUrls.js
+++ src/base/constants/ServiceUrls.js
@@ -1,3 +1,5 @@
+export const GET_CUSTOM_PAYMENTS_LIST='/PaymentsService/getCustomPaymentList';
+export const GET_CUSTOM_TAX_CODES='/PaymentsService/getCustomTaxCodes';
 export const GET_EXISTING_FAVORITE_LINKS='/getExistingFavoriteLinks';
 export const DELETE_EXISTING_FAVORITE_LINKS='/deleteExistingFavoriteLinks';
 export const INSERT_FAVORITE_LINK_RECORD='/insertFavoriteLinkRecord';
