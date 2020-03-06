91e6275d5abf8453fccb4af0233cb3c987cccbfe
Vinit Naik <vnaik@bsihq.com>
2020-03-06 15:15:29 -0500

    Added

--- src/app/actions/griddataAPI.js
+++ src/app/actions/griddataAPI.js
@@ -1,22 +1,38 @@
-
-import {fetchGridDataPending, fetchGridDataSuccess, fetchGridDataError} from './gridDataActions';
-
-const url =  `http://localhost:8000/api/getGridData/`
-
-export function fetchGridData() {
-    return dispatch => {
-        dispatch(fetchGridDataPending());
-        fetch(url)
-        .then(res => res.json())
-        .then(res => { 
-            if(res.error) {
-                throw(res.error);
-            }
-            dispatch(fetchGridDataSuccess(res))
-            return res.details;
-        })
-        .catch(error => {
-            dispatch(fetchGridDataError(error));
-        })
-    }
+import * as svcs from "../../base/constants/ServiceUrls";
+import URLUtils from "../../base/utils/urlUtils";
+import AppError from "../../base/utils/AppError";
+import { ADMIN_ERROR_MSG } from "../../base/utils/AppErrorEvent";
+import { metadatamap, tftools } from "../../base/constants/TFTools";
+class griddataAPI {
+  static getGridData(pageid, getGridDataInput) {
+    let url = griddataAPI.getUrl(pageid);
+    let tt = JSON.stringify(getGridDataInput);
+    return fetch(url, {
+      method: "POST",
+      body: tt,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "same-origin"
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          var errorCode = response.status;
+          var errorMsg = "Unable to get Grid Data Records. " + ADMIN_ERROR_MSG;
+          return new AppError(errorMsg, errorCode);
+        }
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+  static getUrl(pageid) {
+    let metadataMap = metadatamap.find(metadatam => {
+      if (pageid == metadatam.id) return metadatam;
+    });
+    return URLUtils.buildURL(metadataMap.url);
+  }
 }
+export default griddataAPI;
