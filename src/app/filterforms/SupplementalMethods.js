219348e7d78cb68ac3bc81fd458e123450434197
Peter Fullen <peterf@bsihq.com>
2020-03-09 12:27:16 -0400

    this fixes the filter form to show data in the form when filter icon is clicked on grid screen

--- src/app/filterforms/SupplementalMethods.js
+++ src/app/filterforms/SupplementalMethods.js
@@ -12,10 +12,10 @@ class SupplementalMethodsFilterForm extends Component {
     super(props);
 
     this.state = {
-      taxCode: "",
-      taxType: "",
-      formulaNumber: "",
-      startDate: ""
+      taxCode: this.props.formFilterData.taxCode, 
+      taxType: this.props.formFilterData.taxType,
+      formulaNumber: this.props.formFilterData.formulaNo,
+      startDate: this.props.formFilterData.startdate
     };
 
     this.resetForm = () => {
@@ -105,6 +105,7 @@ class SupplementalMethodsFilterForm extends Component {
 function mapStateToProps(state) {
   return {
     data: state.formData.data,
+    formFilterData: state.formFilterData,
     isOpen: state.formData.isOpen,
     index: state.formData
   };
