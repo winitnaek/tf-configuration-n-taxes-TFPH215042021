2a15484a2fa51641c09576c7b9f1957b6bf7a81a
Pete Fullen <fullen_peter@bsi.com>
2020-03-31 11:19:16 -0400

    added renderPage code

---
+++ src/base/utils/renderPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UserDataQueries from '../../app/components/UserDataQueries'
+
+const renderPage = (pgid) => {
+ 
+      form = <UserDataQueries  />
+  return  form;
+};
+
+
+export default renderPage;
