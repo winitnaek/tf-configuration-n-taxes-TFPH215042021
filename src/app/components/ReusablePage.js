1f61b3883154885b6b59bf7400cdbe9ab6cb5aa3
Pete Fullen <fullen_peter@bsi.com>
2020-03-31 14:37:10 -0400

    this converts RenderPage to a functional component and reduces some code

--- src/app/components/ReusablePage.js
+++ src/app/components/ReusablePage.js
@@ -1,28 +1,8 @@
-import React, { Component } from "react";
-import RenderPage from '../../base/utils/renderPage';
+import React, { Component, Fragment } from "react";
+import RenderPage from "../../base/utils/renderPage";
 
-class ReusablePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    const {pgid, help} = this.props;
-
-    this.displayPage = (pgid, help) => {
-        const page = RenderPage(pgid, help)
-      return page;
-    }
-  }
-  render() {
-    const {pgid, help} = this.props;
-    return  (
-        <div>
-       {this.displayPage(pgid, help)}
-
-        </div>
-    )
-
-    
-  }
-} 
-
+const ReusablePage = props => {
+  const { pgid, help } = props;
+  return RenderPage(pgid, help);
+}
 export default ReusablePage;
