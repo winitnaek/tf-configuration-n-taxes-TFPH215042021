eb4f61d0ae9ce4073bf6eb4debe15a471de75d18
Pete Fullen <fullen_peter@bsi.com>
2020-04-01 14:47:14 -0400

    this addes the autocomplete api

---
+++ src/app/api/autocompleteselectAPI.js
@@ -0,0 +1,28 @@
+import AppError from "../../base/utils/AppError";
+import { ADMIN_ERROR_MSG } from "../../base/utils/AppErrorEvent";
+import {autoCompleteUrl, reqInfo} from "../../base/utils/tfUtils";
+
+class autocompleteselectAPI {
+  static getAutoCompleteData(pageid, data, mode) {
+    console.log('Made it to the autocomplete api')
+    console.log(pageid)
+    let url =    autoCompleteUrl()      
+    console.log(url)
+    let tt = JSON.stringify(data);
+    return fetch(url, reqInfo(tt), mode)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          var errorCode = response.status;
+          var errorMsg = "Unable to retrieve Auto Complete Data. " + ADMIN_ERROR_MSG;
+          return new AppError(errorMsg, errorCode);
+        } 
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+}
+
+export default autocompleteselectAPI;
