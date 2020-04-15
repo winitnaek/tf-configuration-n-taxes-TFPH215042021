d9e086b1abab42eb60c7a482656bdadda1a4934d
Pete Fullen <fullen_peter@bsi.com>
2020-04-15 16:04:13 -0400

    this fixes the setting state to the store for filter forms in userDataQ

--- src/app/components/CustomGrid.js
+++ src/app/components/CustomGrid.js
@@ -3,19 +3,25 @@ import {connect} from 'react-redux';
 import { bindActionCreators } from "redux";
 import { tftools } from "../../base/constants/TFTools";
 import {ReusableGrid} from "bsiuilib";
-import { setFilterFormData } from "../actions/filterFormActions";
+import  setFilterFormData  from "../actions/filterFormActions";
 import savegriddataAPI from "../api/savegriddataAPI";
 import * as fieldData from "../metadata/fieldData";
 import * as formMetaData from "../metadata/metaData";
 import gridDataAPI from "../api/griddataAPI";
 import deletegriddataAPI from "../api/deletegriddataAPI";
 import {getRecentUsage} from "../actions/usageActions";
+
 import * as gridStyles from "../../base/constants/AppConstants";
 import autocompleteSelectAPI from "../api/autocompleteselectAPI";
 
 class CustomGrid extends Component {
   render() {
     const {pageid, metadata, pid, permissions, griddata, help, gridProps, formData, getRecentUsage} = this.props;
+
+    const {dispatch } = gridProps;
+    const filterFormAction = data => {
+      dispatch(setFilterFormData(data))
+    }
 
     return(
         <ReusableGrid
@@ -28,6 +34,7 @@ class CustomGrid extends Component {
             gridProps={gridProps}
             tftools={tftools}
             saveGridData={savegriddataAPI}
+            setFilterFormData={filterFormAction}
             deleteGridData={deletegriddataAPI}
             recentUsage={getRecentUsage}
             formMetaData={formMetaData}
