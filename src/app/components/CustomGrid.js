ad6828da4fc1da805a2ca465dddaf1dc8b3fd345
Raj Dangi <rdangi@bsihq.com>
2020-08-06 15:52:11 -0400

    Type 1 and Type 2 screens updates

--- src/app/components/CustomGrid.js
+++ src/app/components/CustomGrid.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component , Fragment} from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { tftools } from "../../base/constants/TFTools";
-import { ReusableGrid } from "bsiuilib";
+import { ReusableGrid, ConfirmModal } from "bsiuilib";
 import { setFilterFormData } from "../actions/filterFormActions";
 import { setFormData } from "../actions/formActions";
 import { getRecentUsage } from "../actions/usageActions";
@@ -15,26 +15,44 @@ class CustomGrid extends Component {
   constructor(props) {
     super(props);
 
-    this.renderGrid = (pgData) => {
+    this.state = {
+      showAlert: false
+    };
+
+    this.renderGrid = pgData => {
       renderTFApplication("pageContainer", pgData);
     };
 
-    this.formAction = (data) => {
-      console.log(data)
+    this.formAction = data => {
+      console.log(data);
       console.log("you made it back to customGrid");
       this.props.setFormData(data);
     };
 
-    this.filterFormAction = (formData) => {
-      console.log('Setting filterForm Data')
-      const mode = "Edit"
-      const index = null
-      const data = {data: this.props.formFilterData , mode, index}
-      console.log(this.props.formFilterData)
-    //  this.props.setFormData(data)    //  This is needed for subsequent edits to get form data
-      this.props.setFilterFormData(formData);  // This is used to make the api call to render the grid
+    this.filterFormAction = formData => {
+      console.log("Setting filterForm Data");
+      const mode = "Edit";
+      const index = null;
+      const data = { data: this.props.formFilterData, mode, index };
+      console.log(this.props.formFilterData);
+      //  this.props.setFormData(data)    //  This is needed for subsequent edits to get form data
+      this.props.setFilterFormData(formData); // This is used to make the api call to render the grid
     };
 
+    this.handleOk = () => {
+      this.setState({
+        showAlert: false
+      });
+    };
+  }
+
+  componentDidMount() {
+    const { metadata, pageid } = this.props;
+    const { pgdef } = metadata(pageid);
+    const { metaInfo } = pgdef;
+    this.setState({
+      showAlert: !!metaInfo
+    });
   }
 
   render() {
@@ -53,31 +71,36 @@ class CustomGrid extends Component {
       formMetaData
     } = this.props;
 
-   
-const {formAction, filterFormAction} = this
+    const { pgdef } = metadata(pageid);
+    const { metaInfo } = pgdef;
+
+    const { formAction, filterFormAction } = this;
     return (
-      <ReusableGrid
-        pageid={pageid}
-        metadata={metadata}
-        pid={pid}
-        permissions={permissions}
-        griddata={griddata}
-        help={help}
-        gridProps={gridProps}
-        tftools={tftools}
-        saveGridData={savegriddataAPI}
-        setFilterFormData={filterFormAction}
-        setFormData={formAction}
-        deleteGridData={deletegriddataAPI}
-        recentUsage={getRecentUsage}
-        renderGrid={this.renderGrid}
-        formMetaData={formMetaData}
-        formData={formData}
-        formFilterData={formFilterData}
-        fieldData={fieldData}
-        autoComplete={autocompleteSelectAPI}
-        styles={gridStyles}
-      />
+      <Fragment>
+        <ReusableGrid
+          pageid={pageid}
+          metadata={metadata}
+          pid={pid}
+          permissions={permissions}
+          griddata={griddata}
+          help={help}
+          gridProps={gridProps}
+          tftools={tftools}
+          saveGridData={savegriddataAPI}
+          setFilterFormData={filterFormAction}
+          setFormData={formAction}
+          deleteGridData={deletegriddataAPI}
+          recentUsage={getRecentUsage}
+          renderGrid={this.renderGrid}
+          formMetaData={formMetaData}
+          formData={formData}
+          formFilterData={formFilterData}
+          fieldData={fieldData}
+          autoComplete={autocompleteSelectAPI}
+          styles={gridStyles}
+        />
+        <ConfirmModal showConfirm={this.state.showAlert} handleOk={this.handleOk} {...metaInfo} />
+      </Fragment>
     );
   }
 }
