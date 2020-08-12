88af8cc92ed3264c6caf3f26c2c1dce584655abe
Raj Dangi <rdangi@bsihq.com>
2020-08-12 18:06:35 -0400

    Resident work validation formula field updates

--- src/base/utils/tfUtils.js
+++ src/base/utils/tfUtils.js
@@ -241,7 +241,7 @@ export function getPmtUsrCode(filterData){
   }
 }
 
-export function buildAutoCompSelInput(pageid, store, patten) {
+export function buildAutoCompSelInput(pageid, store, patten, formValues={}) {
   let state = store.getState();
   console.log(state);
   let input = {
@@ -249,12 +249,9 @@ export function buildAutoCompSelInput(pageid, store, patten) {
     dataset: appDataset(),
     userId: appUserId(),
     pattern: patten,
-    taxCode:"BSI00000000",//samplevalues - update it with value of field from filter
-    taxType:"001",//samplevalues - update it with value of field from filter
-    formula:"1",//samplevalues - update it with value of field from filter
-    startdate:"08/12/2020"//samplevalues - update it with value of field from filter
   };
-  return input;
+  
+  return Object.assign(input, formValues);
 }
 export function buildDeleteInput(pageid, store) {
   let state = store.getState();
